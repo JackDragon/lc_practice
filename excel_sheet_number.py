d336a8647cd399c83614efc1dde1984d153576f9
jackrlong <r.jack.long@gmail.com>
2018-04-10 00:17:24 -0700

    Excel sheet number: slightly faster with enum.

--- excel_sheet_number.py
+++ excel_sheet_number.py
@@ -1,12 +1,21 @@
 class Solution(object):
+    # def titleToNumber(self, s):
+    #     """
+    #     :type s: str
+    #     :rtype: int
+    #     """
+    #     l, n = len(s), 0
+    #     for i in range(l):
+    #         j = s[i]
+    #         # A = 65
+    #         n += (ord(j)-64) * 26 ** (l-i-1)
+    #     return n
     def titleToNumber(self, s):
         """
         :type s: str
         :rtype: int
         """
         l, n = len(s), 0
-        for i in range(l):
-            j = s[i]
-            # A = 65
+        for i,j in enumerate(s):
             n += (ord(j)-64) * 26 ** (l-i-1)
         return n
