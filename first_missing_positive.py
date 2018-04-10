af977e94d77d9e10c4f0dd2dd57b8c9697e4158a
jackrlong <r.jack.long@gmail.com>
2018-04-09 23:57:42 -0700

    test

--- first_missing_positive.py
+++ first_missing_positive.py
@@ -4,11 +4,36 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-def helper(i, nums):
-    j = nums[i]
-    if j < 0:
-        nums[i] = 0
+        for i in range(len(nums)):
+            # print(nums)
+            nums = helper(i, nums)
+        # print(nums)
+        for i in range(len(nums)):
+            if not nums[i]:
+                return i+1
+        return len(nums)+1  
+
+def helper(i, a):
+    # print("helper", i, a)
+    j = a[i]
+    if j == i+1 or j == 0:
+        return a
+    if j < 1 or j > len(a):
+        a[i] = 0
+    # elif j == i:
+    #     a[j] = 0
+    #     a = helper(i, a)
     else:
-        nums = helper(j, nums)
-        nums[j] = j
-    return nums
+        a[j-1], a[i] = j if j > 0 else 0, a[j-1] if a[j-1] > 0 and a[j-1] != a[i] else 0
+        a = helper(i, a)
+    return a
+
+
+print("Should be: 4, 2, 1, 1, 5, 3, 2")
+print(firstMissingPositive([1, 0, -1, 2, 3, 5]))
+print(firstMissingPositive([1]))
+print(firstMissingPositive([-1]))
+print(firstMissingPositive([-1, 0]))
+print(firstMissingPositive([-1, 0, 1, 3, 2, 4, 7]))
+print(firstMissingPositive([2, 1]))
+print(firstMissingPositive([3,4,-1,1]))
