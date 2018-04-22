5706c280d3d4116be37a01705fa06a0db5ee51aa
jackrlong <r.jack.long@gmail.com>
2018-04-21 23:55:42 -0700

    Contest 81 shortest distance done.

--- smallest_rotation_with_highest_score.py
+++ smallest_rotation_with_highest_score.py
@@ -1,17 +1,18 @@
-# Example 1:
-# Input: [2, 3, 1, 4, 0]
-# Output: 3
-# Explanation:  
-# Scores for each K are listed below: 
-# K = 0,  A = [2,3,1,4,0],    score 2
-# K = 1,  A = [3,1,4,0,2],    score 3
-# K = 2,  A = [1,4,0,2,3],    score 3
-# K = 3,  A = [4,0,2,3,1],    score 4
-# K = 4,  A = [0,2,3,1,4],    score 3
-class Solution(object):
-    def bestRotation(self, A):
+class Solution:
+    def shortestToChar(self, S, C):
         """
-        :type A: List[int]
-        :rtype: int
+        :type S: str
+        :type C: str
+        :rtype: List[int]
         """
-        
+        cmap = set()
+        l = []
+        for i in range(len(S)):
+            if S[i] == C:
+                cmap.add(i)
+        for i in range(len(S)):
+            if S[i] == C:
+                l.append(0)
+            else:
+                l.append(min([abs(i - n) for n in cmap]))
+        return l
