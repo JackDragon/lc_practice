ba8de5376d808685c1f9f74d292b7c20ba7f1c3e
jackrlong <r.jack.long@gmail.com>
2018-05-02 23:54:57 -0700

    completed max chunks

--- max_chunks_to_sorted.py
+++ max_chunks_to_sorted.py
@@ -4,4 +4,12 @@ class Solution:
         :type arr: List[int]
         :rtype: int
         """
-        
+        index=[arr[i] for i in sorted(arr)]
+        index=sorted(range(len(arr)), key=arr.__getitem__)
+        print(index)
+        maximum = 0
+        temp = 0
+        for i, x in enumerate(index):
+            temp = max(temp, x)
+            if temp == i: maximum += 1
+        return maximum
