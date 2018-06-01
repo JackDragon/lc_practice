5ee6c8a911caba43c76c6777c319e5cf8ee3c6e9
jackrlong <r.jack.long@gmail.com>
2018-06-01 05:23:50 -0700

    started linked list cycle2 prob

---
+++ linked_list_cycle_2.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        p1 = p2 = head
+        while p1.next and p2.next:
+            p2 = p2.next
+            if p2.next == p1.next:
+                return p1
+            if not p2.next:
+                return
+            p2 = p2.next
+            if p2.next == p1.next:
+                return p1
+            p1 = p1.next
+            if p2.next == p1.next:
+                return p1
+        return
+
+solution = Solution()
+
+n1, n2, n3, n4 = ListNode(1),ListNode(2),ListNode(3),ListNode(1)
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n1
+print(solution.detectCycle(n1))
